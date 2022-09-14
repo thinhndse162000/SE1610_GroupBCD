b8f63c7e47d401d5ac100b20178317860dd3113a
Duy <nguyenduduy233@gmail.com>
2022-09-14 14:08:49 +0700

    Backend: Project Structure

---
+++ backend/src/main/java/com/bcd/ejournal/EjournalApplication.java
@@ -0,0 +1,13 @@
+package com.bcd.ejournal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjournalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjournalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bcd/ejournal/EjournalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bcd.ejournal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EjournalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
