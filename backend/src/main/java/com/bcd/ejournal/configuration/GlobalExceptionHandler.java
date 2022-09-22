4ec1fae7116eaed4de993d9bbf092b997e6022ef
Duy <nguyenduduy233@gmail.com>
2022-09-22 09:48:40 +0700

    Exception handler: Handle `BAD_REQUEST`

--- backend/src/main/java/com/bcd/ejournal/configuration/GlobalExceptionHandler.java
+++ backend/src/main/java/com/bcd/ejournal/configuration/GlobalExceptionHandler.java
@@ -4,12 +4,19 @@ import com.bcd.ejournal.domain.exception.UnauthorizedException;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.ConstraintViolationException;
 import java.time.LocalDateTime;
 import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -40,5 +47,21 @@ public class GlobalExceptionHandler {
         body.put("message", ex.getMessage());
 
         return new ResponseEntity<>(body, HttpStatus.UNAUTHORIZED);
+    }
+    @ExceptionHandler({ MethodArgumentNotValidException.class})
+    public ResponseEntity<Map<String, Object>> yourExceptionHandler(MethodArgumentNotValidException ex) {
+
+        Map<String, Object> body = new LinkedHashMap<>();
+        Map<String, String> messages = new LinkedHashMap<>();
+
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+        for (FieldError fieldError : fieldErrors) {
+            messages.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        body.put("timestamp", LocalDateTime.now());
+        body.put("messages", messages);
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 }
