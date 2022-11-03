64500d653c287367063c0ebeb46b97ca1e007969
thinh2002 <thinhndse16200@fpt.edu.vn>
2022-11-03 20:00:53 +0700

    small fix

--- backend/src/main/java/com/bcd/ejournal/api/PaypalApi.java
+++ backend/src/main/java/com/bcd/ejournal/api/PaypalApi.java
@@ -3,6 +3,8 @@ package com.bcd.ejournal.api;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -31,13 +33,13 @@ public class PaypalApi {
 	public static final String CANCEL_URL = "cancel";
 
 	@PostMapping
-	public String payment(@RequestBody PaypalCheckoutRequest req ) {
+	public ResponseEntity<String> payment(@RequestBody PaypalCheckoutRequest req ) {
 		try {
 			String cancelUrl = CANCEL_URL;
-			Payment payment = service.createPayment(req.getAmount(), "USD", req.getPaymentMethod(), "sale", "laksdf","http://localhost:8080/"+CANCEL_URL, "http://localhost:8080/"+SUCCESS_URL);
+			Payment payment = service.createPayment(req.getAmount(), "USD", req.getPaymentMethod(), "sale", "Buy a journal","http://localhost:8080/"+CANCEL_URL, "http://localhost:8080/"+SUCCESS_URL);
 			for(Links link:payment.getLinks()) {
 				if(link.getRel().equals("approval_url")) {
-					return link.getHref();
+					return new ResponseEntity<>(link.getHref(), HttpStatus.OK);
 				}
 			}
 			
@@ -45,7 +47,7 @@ public class PaypalApi {
 		
 			e.printStackTrace();
 		}
-		return "";
+		return null;
 	}
 	
 	 @GetMapping(value = CANCEL_URL)
@@ -54,7 +56,7 @@ public class PaypalApi {
 	    }
 
 	    @GetMapping(value = SUCCESS_URL)
-	    public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId, @RequestParam("accountId") Integer accountId, @RequestParam("journalId") Integer journalId) {
+	    public ResponseEntity<String> successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId, @RequestParam("accountId") Integer accountId, @RequestParam("journalId") Integer journalId) {
 	        try {
 	            Payment payment = service.executePayment(paymentId, payerId);
 	            System.out.println(payment.toJSON());
@@ -62,12 +64,12 @@ public class PaypalApi {
 	            	double amount =Double.parseDouble(payment.getTransactions().get(0).getAmount().getTotal());
 	            	String method = payment.getPayer().getPaymentMethod();
 	            	invoiceService.createInvoice(amount, method, accountId, journalId);
-	                return "success";
+	                return new ResponseEntity<>("success", HttpStatus.OK);
 	                
 	            }
 	        } catch (PayPalRESTException e) {
 	         System.out.println(e.getMessage());
 	        }
-	        return "";
+	        return null;
 	    }
 }
